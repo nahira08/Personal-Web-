7012e6bf0f25ddb85af2927f324ada2c99c7f4cb
RadyaI <radyaiftikhar@gmail.com>
2025-03-22 12:55:05 +0700

    aduh ketinggalan

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,7 +1,7 @@
 export default function Portfolio() {
     return (
         <>
-            <div className="w-1/2 mx-auto mt-5 mb-30 p-4">
+            <div className="w-[95%] sm:w-1/2 mx-auto mt-5 mb-30 p-4">
                 <div className="flex flex-col items-center gap-2">
                     <p className="font-[--font-ins-serif] text-4xl">Na Portofolio</p>
                     <p className="text-sm">March 22, 2025 - 05:46</p>
